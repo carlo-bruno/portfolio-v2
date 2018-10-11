c587b472f61a6c845b0b6ce0f502fc3e46078d64
Carlo Bruno <carlofbruno@gmail.com>
2018-10-10 20:31:01 -0700

    header changes and base style

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,21 +3,18 @@ import React from "react";
 const Header = () => {
   return (
     <div className="header">
-      <h1>Carlo Bruno | Front-end Web Developer</h1>
-      <ul>
+      <h1>Carlo Bruno</h1>
+      <nav>
         <li>
-          <a href="#">Home</a>
+          <a href="/">About Me</a>
         </li>
         <li>
-          <a href="#">About Me</a>
+          <a href="/">Projects</a>
         </li>
         <li>
-          <a href="#">Projects</a>
+          <a href="/">Contact</a>
         </li>
-        <li>
-          <a href="#">Contact</a>
-        </li>
-      </ul>
+      </nav>
     </div>
   );
 };
