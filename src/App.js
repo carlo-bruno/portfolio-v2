423bc4b4cbdbbbe1ee292a475468f030b5e21903
Carlo Bruno <carlofbruno@gmail.com>
2018-10-10 18:56:03 -0700

    added header, home

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import Header from "./Components/Header";
+
+import Home from "./Pages/Home";
 
 import "./App.css";
 
@@ -6,8 +9,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Carlo Bruno</h1>
-        <h2>Front-end Web Developer</h2>
+        <Header />
+        <Home />
       </div>
     );
   }
---
+++ src/Components/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Carlo Bruno | Front-end Web Developer</h1>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About Me</a>
+        </li>
+        <li>
+          <a href="#">Projects</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Header;
